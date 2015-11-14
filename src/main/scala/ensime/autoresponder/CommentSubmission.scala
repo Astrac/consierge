27d6dfce80717c5a805d025b2e195e3152be1608
Aldo Stracquadanio <aldo.strac@gmail.com>
2015-11-14 14:47:51 +0000

    Complete implementation of comment submission

---
+++ src/main/scala/ensime/autoresponder/CommentSubmission.scala
@@ -0,0 +1,48 @@
+package ensime.autoresponder
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model._
+import akka.stream.Materializer
+import akka.stream.scaladsl.Flow
+import akka.util.ByteString
+import play.api.libs.json.{JsValue, Json}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+trait CommentSubmission extends Transport with Environment {
+  val DownloadTimeout = 500.millis
+  val CommentParallelism = 20
+
+  def createCommentUri(i: Issue) = s"/repos/${config.owner}/${config.repo}/issues/${i.number}/comments"
+
+  def makeJson(i: Issue): JsValue = Json.obj("body" -> config.message)
+
+  def commentRequest(i: Issue): HttpRequest = HttpRequest(
+    method = HttpMethods.POST,
+    uri = Uri(createCommentUri(i))
+      .withHost(Host)
+      .withScheme(Uri.httpScheme(securedConnection = true)),
+    entity = HttpEntity.apply(
+      contentType = ContentTypes.`application/json`,
+      data = ByteString(Json.stringify(makeJson(i)), "UTF-8")
+    )
+  )
+
+  def commentResponse(r: Try[HttpResponse], i: Issue)(implicit mat: Materializer, ec: ExecutionContext): Future[CommentResponse] = r match {
+    case Success(resp) =>
+      resp.entity.toStrict(DownloadTimeout).map { e =>
+        Json.parse(e.data.decodeString("UTF-8")).as[CommentResponse]
+      }
+    case Failure(ex) => throw ex
+  }
+
+  def respondFlow(implicit mat: Materializer, as: ActorSystem, ec: ExecutionContext): Flow[Issue, CommentResponse, Unit] = Flow[Issue]
+    .map(i => (commentRequest(i), i))
+    .via(pool)
+    .mapAsync(CommentParallelism)((commentResponse _).tupled)
+
+}
--- src/main/scala/ensime/autoresponder/model.scala
+++ src/main/scala/ensime/autoresponder/model.scala
@@ -2,20 +2,13 @@ package ensime.autoresponder
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.ContentTypes
-import akka.http.scaladsl.model.HttpMethods
-import akka.http.scaladsl.model.HttpRequest
-import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse, Uri}
 import akka.stream.Materializer
-import akka.stream.scaladsl.Flow
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.util.ByteString
-import org.joda.time.LocalDateTime
-import play.api.libs.json.JsValue
-import play.api.libs.json.Json
+import org.joda.time.DateTime
+import play.api.libs.json.{JsValue, Json}
+import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 case class Configuration(owner: String, repo: String, message: String)
@@ -28,9 +21,20 @@ object IssueState {
 
 case class User(id: Int)
 
-case class Issue(id: Int, number: Int, user: User, title: String, body: String, state: IssueState, createdAt: LocalDateTime, updatedAt: LocalDateTime)
+case class Issue(id: Int, number: Int, user: User, title: String, body: String, state: IssueState, createdAt: DateTime, updatedAt: DateTime)
 
-case class CommentResponse(id: Int, url: String, createdAt: LocalDateTime)
+case class CommentResponse(id: Int, url: String, createdAt: DateTime)
+
+object CommentResponse {
+  import play.api.libs.json._
+  import play.api.libs.functional.syntax._
+
+  implicit val reads: Reads[CommentResponse] = (
+    (JsPath \ "id").read[Int] and
+      (JsPath \ "url").read[String] and
+      (JsPath \ "created_at").read[DateTime]
+  )(CommentResponse.apply _)
+}
 
 trait Environment {
   def config: Configuration
@@ -49,34 +53,10 @@ trait Flows extends CommentSubmission {
       .via(pool)
       .map(handleResponse)
 
-  def graph(implicit mat: Materializer, as: ActorSystem) = issueSource
+  def graph(implicit mat: Materializer, as: ActorSystem, ec: ExecutionContext) = issueSource
     .via(searchFlow)
     .via(respondFlow)
     .to(responseSink)
-}
-
-trait CommentSubmission extends Transport with Environment {
-  def createCommentUri(i: Issue) = s"/repos/${config.owner}/${config.repo}/issues/${i.number}/comments"
-
-  def makeJson(i: Issue): JsValue = Json.obj("body" -> config.message)
-
-  def commentRequest(i: Issue): HttpRequest = HttpRequest(
-    method = HttpMethods.POST,
-    uri = Uri(createCommentUri(i))
-      .withHost(Host)
-      .withScheme(Uri.httpScheme(securedConnection = true)),
-    entity = HttpEntity.apply(
-      contentType = ContentTypes.`application/json`,
-      data = ByteString.apply(Json.stringify(makeJson(i)), "UTF-8"))
-  )
-
-  def commentResponse(r: Try[HttpResponse], i: Issue): CommentResponse
-
-  def respondFlow(implicit mat: Materializer, as: ActorSystem): Flow[Issue, CommentResponse, Unit] = Flow[Issue]
-    .map(i => (commentRequest(i), i))
-    .via(pool)
-    .map((commentResponse _).tupled)
-
 }
 
 trait Transport {
